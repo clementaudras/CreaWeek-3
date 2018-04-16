5750aa1597ac80335b20a0c858ef40ce645517fe
clementaudras <clement.audras@gmail.com>
2018-04-16 14:49:01 +0200

    project 2017.4.1

---
+++ Projet_SemaineCrea#3/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public Transform arrow;
+	public Transform target;
+
+	public float horizontalSpeed = 100.0F;
+	    public Rigidbody2D rb;
+
+		void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+		 transform.LookAt(target);
+
+		//debug
+		if (Input.GetKey(KeyCode.Q))
+			transform.Rotate(Vector3.forward * horizontalSpeed * Time.deltaTime);
+      
+		if (Input.GetKey(KeyCode.D))
+			transform.Rotate(-Vector3.forward * horizontalSpeed * Time.deltaTime);
+
+		//Xbox 360 controller 1
+
+		//Xbox 360 controller 2
+		if(Input.GetKeyDown(KeyCode.T)){
+			Debug.Log("T");
+			rb.AddRelativeForce(Vector3.forward * 100f);
+				  /*
+			Quaternion rotation = Quaternion.Euler(0, 0, 45);
+			Vector3 direction = rotation * Vector3.forward;
+			rb.AddForce(direction * 100f);
+			rb.velocity = direction * 100f;
+			
+			//rb.AddRelativeForce(Vector3.forward * 100f);
+			//rb.AddForce(transform.right * 10f);
+			*/
+		}
+				//Debug.DrawLine(transform.position, target * 100f, Color.red);
+	}
+}
+	 
+	 
---
+++ Projet_SemaineCrea#3/Assets/Scripts/Unity test/Reflect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reflect : MonoBehaviour {
+
+public Transform originalObject;
+    public Transform reflectedObject;
+    void Update() {
+        reflectedObject.position = Vector3.Reflect(originalObject.position, Vector3.right);
+    }
+}
