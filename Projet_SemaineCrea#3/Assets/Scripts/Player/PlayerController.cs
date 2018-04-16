249dd652d84d626375c8dfdeb43cc1dbce17380f
clementaudras <clement.audras@gmail.com>
2018-04-16 16:00:54 +0200

    dash
    dash

--- Projet_SemaineCrea#3/Assets/Scripts/Player/PlayerController.cs
+++ Projet_SemaineCrea#3/Assets/Scripts/Player/PlayerController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	public Transform arrow;
-	public Transform target;
+		public Transform arrow;
+		public Transform target;
 
-	public float horizontalSpeed = 100.0F;
+		public float horizontalSpeed = 100.0F;
 	    public Rigidbody2D rb;
+		public float forcePower = 50f;
+		public bool slow = false;
 
 		void Start()
     {
@@ -15,21 +17,40 @@ public class PlayerController : MonoBehaviour {
     }
 
     void Update() {
-		 transform.LookAt(target);
-
 		//debug
+		        //Vector3 targetDir = target.position - transform.position;
+
+		//Vector3 newDir = Vector3.RotateTowards(transform.right, targetDir, 0f, 0.0f);
+        //Debug.DrawRay(transform.position, newDir, Color.red);
+
 		if (Input.GetKey(KeyCode.Q))
-			transform.Rotate(Vector3.forward * horizontalSpeed * Time.deltaTime);
+			arrow.Rotate(Vector3.forward * horizontalSpeed * Time.deltaTime);
       
 		if (Input.GetKey(KeyCode.D))
-			transform.Rotate(-Vector3.forward * horizontalSpeed * Time.deltaTime);
+			arrow.Rotate(-Vector3.forward * horizontalSpeed * Time.deltaTime);
+
+
+
+				//Debug.DrawLine(transform.position, newDir * 100f, Color.red);
 
 		//Xbox 360 controller 1
-
+					if(slow){
+						rb.velocity = rb.velocity * 0.9f;
+					}
+					
+					if(Input.GetKey(KeyCode.S)){
+				Debug.Log(rb.velocity);
+				//rb.velocity = Vector3.zero;
+				rb.velocity = rb.velocity * 0.9f;
+			}
 		//Xbox 360 controller 2
 		if(Input.GetKeyDown(KeyCode.T)){
 			Debug.Log("T");
-			rb.AddRelativeForce(Vector3.forward * 100f);
+			//rb.AddRelativeForce(Vector3.forward * 100f);
+			rb.AddRelativeForce((target.transform.position - transform.position).normalized * forcePower);
+			slow = true;
+
+			//rb.AddRelativeForce((target.transform.position - transform.position).normalized * -40);
 				  /*
 			Quaternion rotation = Quaternion.Euler(0, 0, 45);
 			Vector3 direction = rotation * Vector3.forward;
@@ -39,8 +60,20 @@ public class PlayerController : MonoBehaviour {
 			//rb.AddRelativeForce(Vector3.forward * 100f);
 			//rb.AddForce(transform.right * 10f);
 			*/
+
+			//reflectedObject.position = Vector3.Reflect(originalObject.position, Vector3.right);
+
+
+			/*
+						if(slow){
+				rb.velocity.x = rb.velocity.x * -1f * Time.deltaTime;
+								rb.velocity.y = rb.velocity.y * -1f * Time.deltaTime;
+			if(rb.velocity <= 0){
+				rb.velocity = 0f;
+				}
+			}
+			*/
 		}
-				//Debug.DrawLine(transform.position, target * 100f, Color.red);
 	}
 }
 	 
