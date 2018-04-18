add8fb90cebb8bd526f8134afadcb9c4dd79b3ea
benjilap <benjilap2@yahoo.fr>
2018-04-18 16:07:41 +0200

    up sound

---
+++ Projet_SemaineCrea#3/Assets/Scripts/SoundScript/PouleSound.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PouleSound : MonoBehaviour {
+
+    public AudioClip coursePoule;
+    private float velToVol = .2f;
+
+
+    AudioSource speakerPoule;
+    Rigidbody2D rb;
+
+    void Start() {
+        speakerPoule = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float runVol = velToVol * rb.velocity.magnitude;
+        if (rb.velocity.magnitude > 0.9)
+        {
+
+            speakerPoule.Play();
+            speakerPoule.clip=coursePoule;
+            speakerPoule.loop = true;
+        }
+        else
+        {
+            speakerPoule.loop = false;
+            speakerPoule.Stop();
+        }
+
+    }
+
+    IEnumerator runSound()
+        { 
+            yield return null; }
+}
