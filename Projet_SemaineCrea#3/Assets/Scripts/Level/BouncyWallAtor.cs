3a8ed0db52ad44ae480d1f975b2ff40f570cf739
benjilap <benjilap2@yahoo.fr>
2018-04-19 10:18:35 +0200

    up bounce wall

---
+++ Projet_SemaineCrea#3/Assets/Scripts/Level/BouncyWallAtor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyWallAtor : MonoBehaviour {
+
+    Animator BouncyAtor;
+
+	void Awake () {
+        BouncyAtor = this.GetComponent<Animator>();
+	}
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        BouncyAtor.SetBool("Bounce",true);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        BouncyAtor.SetBool("Bounce", false);
+    }
+}
