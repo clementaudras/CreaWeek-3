027827ef3f7a8b7ad3f0b611ebcb33df8097ca06
ClementA <clement.audras@gmail.com>
2018-05-28 12:49:04 +0200

    Levels Selection WIP

---
+++ Projet_SemaineCrea#3/Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class LevelSelection : MonoBehaviour {
+    public int numOfCtrlrs;
+
+    // Update is called once per frame
+    void Update () {
+        int numOfCtrlrs = XCI.GetNumPluggedCtrlrs();
+        Debug.Log("There are " + numOfCtrlrs + " Xbox controllers plugged in.");
+
+        //Instructions
+        if (numOfCtrlrs < 2)
+        {
+            Debug.Log("Need at least two players to play.");
+        }
+
+        //Levels
+        if (numOfCtrlrs == 2)
+        {
+            Debug.Log("2 players levels are loaded.");
+        }
+
+        if (numOfCtrlrs == 3)
+        {
+            Debug.Log("3 players levels are loaded.");
+        }
+
+        if (numOfCtrlrs == 4)
+        {
+            Debug.Log("4 players levels are loaded.");
+        }
+    }
+}
---
+++ Projet_SemaineCrea#3/Assets/Scripts/PlayerSelection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class PlayerSelection : MonoBehaviour {
+    [HideInInspector] public int numOfCtrlrs;
+
+    public GameObject spriteP1;
+    public GameObject spriteP2;
+    public GameObject spriteP3;
+    public GameObject spriteP4;
+
+    // Update is called once per frame
+    void Update () {
+        int numOfCtrlrs = XCI.GetNumPluggedCtrlrs();
+        Debug.Log("There are " + numOfCtrlrs + " Xbox controllers plugged in.");
+
+        //Debug
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            numOfCtrlrs += 1;
+        }
+
+        if (numOfCtrlrs == 1)
+        {
+            spriteP1.SetActive(true);
+        } else if (numOfCtrlrs == 2)
+        {
+            spriteP2.SetActive(true);
+        } else if(numOfCtrlrs == 3)
+        {
+            spriteP3.SetActive(true);
+        } else if(numOfCtrlrs == 4)
+        {
+            spriteP4.SetActive(true);
+        }
+    }
+}
